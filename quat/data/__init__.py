67ac97c0e411f58ea413c8c337f1cf74e428ca1f
gealachlee <592441537@qq.com>
2026-06-21 20:17:57 +0800

    feat: migrate data module to quat.data

---
+++ quat/data/__init__.py
@@ -0,0 +1,9 @@
+# Quaternion SMM Data Module
+
+from quat.data.loader import (
+    load_cifar10_samples,
+    load_stl10_samples,
+    load_flower17_samples,
+    load_imagenette_samples,
+    load_svhn_samples,
+)
